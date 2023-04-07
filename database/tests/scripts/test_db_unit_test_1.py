a893db73994c34de74058d776f9d3a031691a084
Wahhaj Javed <14081651+wahhajjaved@users.noreply.github.com>
2023-04-07 00:49:26 -0600

    Docker compose file changed to spawn two containers. Database connection does not work yet.

--- database/tests/scripts/test_db_unit_test_1.py
+++ database/tests/scripts/test_db_unit_test_1.py
@@ -1,7 +1,9 @@
 import pymysql
 import pytest
-from test_all import db_connection
+from test_all import testdb2_connection
 
-def test_db_unit_test_1(db_connection):
+def test_db_unit_test_1(testdb1_connection, testdb2_connection):
 
-	assert db_connection is not None, "Failed to establish a connection with the database"
+	assert testdb1_connection is not None, "Failed to establish a connection with the database testdb1"
+	assert testdb2_connection is not None, "Failed to establish a connection with the database testdb2"
+
